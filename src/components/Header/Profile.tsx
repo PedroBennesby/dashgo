519db80e3b0da3b51a19010ef60f70bcdb1939ae
Pedro Bennesby <pedrobennesby@gmail.com>
2021-12-21 15:48:48 -0300

    refactor(Responsive): :art: Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Flex, Box, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface profileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: profileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Pedro Bennesby</Text>
-        <Text color='gray.300' fontSize='small'>
-          pedrobennesby@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Pedro Bennesby</Text>
+          <Text color='gray.300' fontSize='small'>
+            pedrobennesby@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size='md' name='Pedro Bennesby' />
     </Flex>
