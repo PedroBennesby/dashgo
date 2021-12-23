46776f45deb35c50d84c10f5ad7da12439813dd3
Pedro Bennesby <pedrobennesby@gmail.com>
2021-12-23 17:01:36 -0300

    feat(Component): :sparkles: Form validation

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,18 +1,25 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChrakraInputProps,
 } from '@chakra-ui/react';
+import { FieldError } from 'react-hook-form';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
 interface InputProps extends ChrakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, type, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, type, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         id={name}
@@ -22,8 +29,12 @@ export function Input({ name, label, type, ...rest }: InputProps) {
         variant='filled'
         _hover={{ bgColor: 'gray.900' }}
         size='lg'
+        ref={ref}
         {...rest}
       ></ChakraInput>
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
